1393e880e5f52382cd5d7461f290bad25c1c06f6
Peter Chen <peterchen2017125@gmail.com>
2024-11-08 21:07:08 +0800

    remove: unused service

--- src/upload-carbon/upload-carbon.controller.ts
+++ src/upload-carbon/upload-carbon.controller.ts
@@ -1,14 +1,10 @@
-import { JwtDecrypTool } from '@/common';
 import { Body, Controller, Post } from '@nestjs/common';
 import { UploadCarbonListingDto } from './dto/upload-carbon-listing.dto';
 import { UploadCarbonService } from './upload-carbon.service';
 
 @Controller('/carbon')
 export class UploadCarbonController {
-  constructor(
-    private readonly uploadCarbonService: UploadCarbonService,
-    private readonly jwtDecrypTool: JwtDecrypTool,
-  ) {}
+  constructor(private readonly uploadCarbonService: UploadCarbonService) {}
   @Post('upload-carbon')
   uploadCarbon(@Body() uploadCarbonListingDto: UploadCarbonListingDto) {
     return this.uploadCarbonService.uploadCarbon(uploadCarbonListingDto);
